db5506dc32c63f1b5ccaa19696209dda78d26303
Collin Woodruff <Cwoodruff@mail.greenriver.edu>
2020-04-29 18:08:40 -0400

    Created the general files git push

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Collin Woodruff
+ * Date: 1/14/2019
+ * Time: 10:10 AM
+ */
+
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+//Require autoload
+require_once('vendor/autoload.php');
+
+session_start();
+
+//Create an instance of the Base Class
+$f3 = Base::instance();
+
+//Turn on Fat-Free error reporting
+$f3->set('DEBUG', 3);
+
+//Define a default route
+$f3->route('GET /', function() {
+    $view = new Template();
+    echo $view->render('views/petHome.html');
+});
+
+$f3->route('GET|POST /order', function() {
+    var_dump($_POST);
+    $view = new Template();
+    echo $view->render('views/order.html');
+});
+
+//Run fat free
+$f3->run();
